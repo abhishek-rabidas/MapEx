1dfc8bf9cdfb5932c1760d2f6e9f257d32369aab
abhishek-rabidas <avisec07@gmail.com>
2023-11-02 20:18:34 +0530

    project init

---
+++ src/main/java/abhishek/mapex/MapexApplication.java
@@ -0,0 +1,13 @@
+package abhishek.mapex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abhishek/mapex/MapexApplicationTests.java
@@ -0,0 +1,13 @@
+package abhishek.mapex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
