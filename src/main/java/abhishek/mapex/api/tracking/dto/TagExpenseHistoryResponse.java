19922c8db13f763762a3e5a6915df783f2050f1d
abhishek-rabidas <avisec07@gmail.com>
2023-11-05 17:31:31 +0530

    tag tracking most expensive amount and date

--- src/main/java/abhishek/mapex/api/tracking/dto/TagExpenseHistoryResponse.java
+++ src/main/java/abhishek/mapex/api/tracking/dto/TagExpenseHistoryResponse.java
@@ -1,13 +1,14 @@
 package abhishek.mapex.api.tracking.dto;
 
-import abhishek.mapex.api.message.dao.Message;
 import abhishek.mapex.api.message.dao.Tag;
 import abhishek.mapex.api.message.dto.MessageResponse;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Getter
 @Setter
@@ -21,9 +22,22 @@ public class TagExpenseHistoryResponse {
     private List<MessageResponse> history;
 
     public TagExpenseHistoryResponse(Tag tag) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
         this.tag = tag.getName();
         this.totalSpent = tag.getTotalSpend();
         this.usageTimes = tag.getUseCount();
-        this.history = tag.getMessages().stream().map(MessageResponse::new).toList();
+
+        AtomicReference<Double> maxAmount = new AtomicReference<>((double) 0);
+        AtomicReference<String> mostExpensiveDate = new AtomicReference<>("");
+        this.history = tag.getMessages().stream().map(message -> {
+            if (message.getAmount() > maxAmount.get()) {
+                maxAmount.set(message.getAmount());
+                mostExpensiveDate.set(dateFormat.format(message.getCreatedOn()));
+            }
+            return new MessageResponse(message);
+        }).toList();
+
+        this.mostExpensiveAmount = maxAmount.get();
+        this.mostExpensiveDate = mostExpensiveDate.get();
     }
 }
